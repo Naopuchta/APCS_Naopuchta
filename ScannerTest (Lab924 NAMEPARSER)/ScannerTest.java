26ce0ab6d1e23279913fcb012367b98d72284fa4
naompuchta <54680178+naompuchta@users.noreply.github.com>
2019-09-26 10:27:48 -0700

    changes

---
+++ ScannerTest (Lab924 NAMEPARSER)/ScannerTest.java
@@ -0,0 +1,27 @@
+
+/**
+ * Scanner tester
+ *
+ * @author (eettlin)
+ * @version (924)
+ */
+
+import java.util.Scanner;
+public class ScannerTest{
+    
+    
+    public static void main(){
+        Scanner kb = new Scanner(System.in);
+        String inputStr = "";
+        //  if(str1 == str2)
+        while(!inputStr.equals("quit")){
+            System.out.println("Enter a name or type \"quit\" to quit");
+            inputStr = kb.nextLine();
+            // add in where it links them to the other person
+            Person.fullName = inputStr;
+            System.out.println(inputStr);
+        }
+        
+        
+    }
+}
