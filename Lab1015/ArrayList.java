e1c09a950ad7b11ee5a6c6f620a80c4fa411c3da
Naopuchta <54371501+Naopuchta@users.noreply.github.com>
2019-10-15 10:45:37 -0700

    changes

---
+++ Lab1015/ArrayList.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class ArrayList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ArrayList
+{
+    // instance variables - replace the example below with your own
+    ArrayList<int>numberList;
+    int words = new ArrayList<int>();  
+    
+    /**
+     * Constructor for objects of class ArrayList
+     */
+    public ArrayListing()
+    {
+        
+    }
+}
