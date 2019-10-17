dc858ac0f0c01a2d70945d24da6d73606d1c443d
Naopuchta <54371501+Naopuchta@users.noreply.github.com>
2019-10-17 10:28:52 -0700

    changes

---
+++ Lab1017/BubbleSort.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class BubbleSort here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BubbleSort
+{
+    // instance variables - replace the example below with your own
+    String[] numberList = {"1","4","6","8","3","5","9"};
+
+    /**
+     * Constructor for objects of class BubbleSort
+     */
+    public BubbleSort()
+    {
+        for(int i = 0; i < numberList.length(); i++){
+            for(int j = 0; j < i; j++){
+            
+            }
+        }
+    }
+
+}
--- NestedLoopsPractice/NestedLoops.java
+++ NestedLoopsPractice/NestedLoops.java
@@ -8,12 +8,13 @@
 public class NestedLoops
 {
     // instance variables - replace the example below with your own
-
+    int num = 0;
     /**
      * Constructor for objects of class NestedLoops
      */
     public NestedLoops()
     {
+        // this is for problem A 
         System.out.println("Here is A.");
         System.out.println();
         for(int i = 1; i < 6; i++){
@@ -21,6 +22,7 @@ public class NestedLoops
                 System.out.print(i);
             }
         }
+        // this is for problem B 
         System.out.println("Here is B.");
         System.out.println();
         for(int i = 5; i > 1; i--){
@@ -28,7 +30,23 @@ public class NestedLoops
                 System.out.print(i);
             }
         }
+        // this is problem C
         System.out.println("Here is C.");
         System.out.println();
+        for(int i = 3; i > 0; i++){
+            if(num == 0){
+                num = 2;
+            }
+            else if(num == 2){
+                num = 3;
+            }
+            else if(num == 3){
+                num = 6;
+            }
+            for(int j = 0; j < num; j++){
+
+                System.out.print(num); 
+            }
+        }
     }
 }
