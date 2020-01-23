635c7102eacac959ae7dd6e18566ebf698f3d4e1
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2020-01-23 07:27:13 -0800

    some changes?

--- Semester 2 Projects (2020)/CelebStudentCode/Celebrity.java
+++ Semester 2 Projects (2020)/CelebStudentCode/Celebrity.java
@@ -3,68 +3,74 @@
  * @author cody.henrichsen
  * @version 1.4 17/09/2018
  */
+import java.util.ArrayList;
 public class Celebrity
 {
-	/**
-	 * The clue to determine the celebrity
-	 */
-	
-	/**
-	 * The answer or name of the celebrity.
-	 */
-	
-	/**
-	 * Creates a Celebrity instance with the supplied answer and clue
-	 * @param answer
-	 * @param clue
-	 */
-	public Celebrity(String answer, String clue)
-	{
-	}
+    private String currentCeleb; 
+    private String currentClue;
+    private String userInput;
+    private int rightOrWrong;
+    
+     /**
+     * The clue to determine the celebrity
+     */
+    
+    /**
+     * The answer or name of the celebrity.
+     */
+    
+    /**
+     * Creates a Celebrity instance with the supplied answer and clue
+     * @param answer
+     * @param clue
+     */
+    public Celebrity(String answer, String clue)
+    {
+    }
 
-	/**
-	 * Supplies the clue for the celebrity
-	 * @return
-	 */
-	public String getClue()
-	{
-		return null;
-	}
+    /**
+     * Supplies the clue for the celebrity
+     * @return
+     */
+    public String getClue()
+    {
+        return null;
+    }
 
-	/**
-	 * Supplies the answer for the celebrity.
-	 * @return
-	 */
-	public String getAnswer()
-	{
-		return null;
-	}
+    /**
+     * Supplies the answer for the celebrity.
+     * @return
+     */
+    public String getAnswer()
+    {
+        return null;
+    }
 
-	/**
-	 * Updates the clue to the provided String.
-	 * @param clue The new clue.
-	 */
-	public void setClue(String clue)
-	{
-		
-	}
+    /**
+     * Updates the clue to the provided String.
+     * @param clue The new clue.
+     */
+    public void setClue(String clue)
+    {
+        
+    }
 
-	/**
-	 * Updates the answer to the provided String.
-	 * @param answer The new answer.
-	 */
-	public void setAnswer(String answer)
-	{
-		
-	}
-	
-	/**
-	 * Provides a String representation of the Celebrity.
-	 */
-	@Override
-	public String toString()
-	{
-		return null;
-	}
-	
+    /**
+     * Updates the answer to the provided String.
+     * @param answer The new answer.
+     */
+    public void setAnswer(String answer)
+    {
+        
+    }
+    
+    /**
+     * Provides a String representation of the Celebrity.
+     */
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+    
 }
