dc07a95073d4e03da68f11005ba576b2f0e35859
Naopuchta <54371501+Naopuchta@users.noreply.github.com>
2020-01-31 10:28:02 -0800

    changes

---
+++ Semester 2 Projects (2020)/Elevens/Activity Starter Code/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,15 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card card1 = new Card("5","Hearts",5);
+		Card card2 = new Card("10","Spades",10);
+		Card card3 = new Card("7","Clubs",7);
+	}
+}
---
+++ Semester 2 Projects (2020)/Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
@@ -0,0 +1,14 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Deck deck = new Deck(["1","2","3"],
+		["Spades","Hearts","Diamonds"],[1,2,3]);
+	}
+}
