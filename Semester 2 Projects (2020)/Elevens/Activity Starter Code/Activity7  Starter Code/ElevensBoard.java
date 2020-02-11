a6ad5a631dd92bf54d29dea0f34d039f7185e940
NaPuchta <43476735+NaPuchta@users.noreply.github.com>
2020-02-10 23:39:55 -0800

    changes

--- Semester 2 Projects (2020)/Elevens/Activity Starter Code/Activity7  Starter Code/ElevensBoard.java
+++ Semester 2 Projects (2020)/Elevens/Activity Starter Code/Activity7  Starter Code/ElevensBoard.java
@@ -1,3 +1,27 @@
+// Name: Naomi Puchta 
+// Date: 2/10/2020
+// Version: Activity 7 
+
+// Questions: 
+
+// 1. I would need cards, myself and a few other players in order to play.
+// Examples of instance variables would be cards and player. 
+
+// 2. Not really a precise algorithm but..
+// --> insert code to start the game and auto shuffle
+// --> insert the user input command that would allow the user to pick a card
+// --> database so that the cards may be picked
+// --> removal function to remove chosen cards
+// --> function to check to see if any cards can make 11
+
+// 3. I believe so.
+
+// chose to verbally do the rest of the questions and move on to possibly 
+// getting some of the work done now as is expected. 
+
+// cannot find the print cards method? not really sure if this is just an
+// error on my half but I will likely wait for this until next time. 
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -6,208 +30,222 @@ import java.util.ArrayList;
  */
 public class ElevensBoard {
 
-	/**
-	 * The size (number of cards) on the board.
-	 */
-	private static final int BOARD_SIZE = 9;
+    /**
+     * The size (number of cards) on the board.
+     */
+    private static final int BOARD_SIZE = 9;
 
-	/**
-	 * The ranks of the cards for this game to be sent to the deck.
-	 */
-	private static final String[] RANKS =
-		{"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
+    /**
+     * The ranks of the cards for this game to be sent to the deck.
+     */
+    private static final String[] RANKS =
+        {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
 
-	/**
-	 * The suits of the cards for this game to be sent to the deck.
-	 */
-	private static final String[] SUITS =
-		{"spades", "hearts", "diamonds", "clubs"};
+    /**
+     * The suits of the cards for this game to be sent to the deck.
+     */
+    private static final String[] SUITS =
+        {"spades", "hearts", "diamonds", "clubs"};
 
-	/**
-	 * The values of the cards for this game to be sent to the deck.
-	 */
-	private static final int[] POINT_VALUES =
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
-
-
-	/**
-	 * The cards on this board.
-	 */
-	private Card[] cards;
-
-	/**
-	 * The deck of cards being used to play the current game.
-	 */
-	private Deck deck;
-
-	/**
-	 * Flag used to control debugging print statements.
-	 */
-	private static final boolean I_AM_DEBUGGING = false;
+    /**
+     * The values of the cards for this game to be sent to the deck.
+     */
+    private static final int[] POINT_VALUES =
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
 
 
-	/**
-	 * Creates a new <code>ElevensBoard</code> instance.
-	 */
-	public ElevensBoard() {
-		cards = new Card[BOARD_SIZE];
-		deck = new Deck(RANKS, SUITS, POINT_VALUES);
-		if (I_AM_DEBUGGING) {
-			System.out.println(deck);
-			System.out.println("----------");
-		}
-		dealMyCards();
-	}
+    /**
+     * The cards on this board.
+     */
+    private Card[] cards;
 
-	/**
-	 * Start a new game by shuffling the deck and
-	 * dealing some cards to this board.
-	 */
-	public void newGame() {
-		deck.shuffle();
-		dealMyCards();
-	}
+    /**
+     * The deck of cards being used to play the current game.
+     */
+    private Deck deck;
 
-	/**
-	 * Accesses the size of the board.
-	 * Note that this is not the number of cards it contains,
-	 * which will be smaller near the end of a winning game.
-	 * @return the size of the board
-	 */
-	public int size() {
-		return cards.length;
-	}
-
-	/**
-	 * Determines if the board is empty (has no cards).
-	 * @return true if this board is empty; false otherwise.
-	 */
-	public boolean isEmpty() {
-		for (int k = 0; k < cards.length; k++) {
-			if (cards[k] != null) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * Deal a card to the kth position in this board.
-	 * If the deck is empty, the kth card is set to null.
-	 * @param k the index of the card to be dealt.
-	 */
-	public void deal(int k) {
-		cards[k] = deck.deal();
-	}
-
-	/**
-	 * Accesses the deck's size.
-	 * @return the number of undealt cards left in the deck.
-	 */
-	public int deckSize() {
-		return deck.size();
-	}
-
-	/**
-	 * Accesses a card on the board.
-	 * @return the card at position k on the board.
-	 * @param k is the board position of the card to return.
-	 */
-	public Card cardAt(int k) {
-		return cards[k];
-	}
-
-	/**
-	 * Replaces selected cards on the board by dealing new cards.
-	 * @param selectedCards is a list of the indices of the
-	 *        cards to be replaced.
-	 */
-	public void replaceSelectedCards(List<Integer> selectedCards) {
-		for (Integer k : selectedCards) {
-			deal(k.intValue());
-		}
-	}
-
-	/**
-	 * Gets the indexes of the actual (non-null) cards on the board.
-	 *
-	 * @return a List that contains the locations (indexes)
-	 *         of the non-null entries on the board.
-	 */
-	public List<Integer> cardIndexes() {
-		List<Integer> selected = new ArrayList<Integer>();
-		for (int k = 0; k < cards.length; k++) {
-			if (cards[k] != null) {
-				selected.add(new Integer(k));
-			}
-		}
-		return selected;
-	}
-
-	/**
-	 * Generates and returns a string representation of this board.
-	 * @return the string version of this board.
-	 */
-	public String toString() {
-		String s = "";
-		for (int k = 0; k < cards.length; k++) {
-			s = s + k + ": " + cards[k] + "\n";
-		}
-		return s;
-	}
-
-	/**
-	 * Determine whether or not the game has been won,
-	 * i.e. neither the board nor the deck has any more cards.
-	 * @return true when the current game has been won;
-	 *         false otherwise.
-	 */
-	public boolean gameIsWon() {
-		if (deck.isEmpty()) {
-			for (Card c : cards) {
-				if (c != null) {
-					return false;
-				}
-			}
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Determines if the selected cards form a valid group for removal.
-	 * In Elevens, the legal groups are (1) a pair of non-face cards
-	 * whose values add to 11, and (2) a group of three cards consisting of
-	 * a jack, a queen, and a king in some order.
-	 * @param selectedCards the list of the indices of the selected cards.
-	 * @return true if the selected cards form a valid group for removal;
-	 *         false otherwise.
-	 */
-	public boolean isLegal(List<Integer> selectedCards) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
-	}
-
-	/**
-	 * Determine if there are any legal plays left on the board.
-	 * In Elevens, there is a legal play if the board contains
-	 * (1) a pair of non-face cards whose values add to 11, or (2) a group
-	 * of three cards consisting of a jack, a queen, and a king in some order.
-	 * @return true if there is a legal play left on the board;
-	 *         false otherwise.
-	 */
-	public boolean anotherPlayIsPossible() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
-	}
+    /**
+     * Flag used to control debugging print statements.
+     */
+    private static final boolean I_AM_DEBUGGING = false;
 
 
-	/**
-	 * Deal cards to this board to start the game.
-	 */
-	private void dealMyCards() {
-		for (int k = 0; k < cards.length; k++) {
-			cards[k] = deck.deal();
-		}
-	}
+    public static printCards(ElevensBoard board) {
+        List<Integer> cIndexes = board.cardIndexes();
+        // temporary 
+        return board; 
+    }
+    /**
+     * Creates a new <code>ElevensBoard</code> instance.
+     */
+    public ElevensBoard() {
+        cards = new Card[BOARD_SIZE];
+        deck = new Deck(RANKS, SUITS, POINT_VALUES);
+        if (I_AM_DEBUGGING) {
+            System.out.println(deck);
+            System.out.println("----------");
+        }
+        dealMyCards();
+    }
+
+    /**
+     * Start a new game by shuffling the deck and
+     * dealing some cards to this board.
+     */
+    public void newGame() {
+        deck.shuffle();
+        dealMyCards();
+    }
+
+    /**
+     * Accesses the size of the board.
+     * Note that this is not the number of cards it contains,
+     * which will be smaller near the end of a winning game.
+     * @return the size of the board
+     */
+    public int size() {
+        return cards.length;
+    }
+
+    /**
+     * Determines if the board is empty (has no cards).
+     * @return true if this board is empty; false otherwise.
+     */
+    public boolean isEmpty() {
+        for (int k = 0; k < cards.length; k++) {
+            if (cards[k] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Deal a card to the kth position in this board.
+     * If the deck is empty, the kth card is set to null.
+     * @param k the index of the card to be dealt.
+     */
+    public void deal(int k) {
+        cards[k] = deck.deal();
+    }
+
+    /**
+     * Accesses the deck's size.
+     * @return the number of undealt cards left in the deck.
+     */
+    public int deckSize() {
+        return deck.size();
+    }
+
+    /**
+     * Accesses a card on the board.
+     * @return the card at position k on the board.
+     * @param k is the board position of the card to return.
+     */
+    public Card cardAt(int k) {
+        return cards[k];
+    }
+
+    /**
+     * Replaces selected cards on the board by dealing new cards.
+     * @param selectedCards is a list of the indices of the
+     *        cards to be replaced.
+     */
+    public void replaceSelectedCards(List<Integer> selectedCards) {
+        for (Integer k : selectedCards) {
+            deal(k.intValue());
+        }
+    }
+
+    /**
+     * Gets the indexes of the actual (non-null) cards on the board.
+     *
+     * @return a List that contains the locations (indexes)
+     *         of the non-null entries on the board.
+     */
+    public List<Integer> cardIndexes() {
+        // for question e. 
+        // I would assume that it would need to check the whole list and
+        // combo them together or something of the sort?
+        // thinking about it right now there should be a lot of combinations
+        // which would make the code very difficult.
+        
+        // I need to think of a way to make things very simple, maybe do
+        // a base bubble swap, without the swap method, and check through
+        // every one to see if there can be an 11's made? 
+        List<Integer> selected = new ArrayList<Integer>();
+        for (int k = 0; k < cards.length; k++) {
+            if (cards[k] != null) {
+                selected.add(new Integer(k));
+            }
+        }
+        return selected;
+    }
+
+    /**
+     * Generates and returns a string representation of this board.
+     * @return the string version of this board.
+     */
+    public String toString() {
+        String s = "";
+        for (int k = 0; k < cards.length; k++) {
+            s = s + k + ": " + cards[k] + "\n";
+        }
+        return s;
+    }
+
+    /**
+     * Determine whether or not the game has been won,
+     * i.e. neither the board nor the deck has any more cards.
+     * @return true when the current game has been won;
+     *         false otherwise.
+     */
+    public boolean gameIsWon() {
+        if (deck.isEmpty()) {
+            for (Card c : cards) {
+                if (c != null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determines if the selected cards form a valid group for removal.
+     * In Elevens, the legal groups are (1) a pair of non-face cards
+     * whose values add to 11, and (2) a group of three cards consisting of
+     * a jack, a queen, and a king in some order.
+     * @param selectedCards the list of the indices of the selected cards.
+     * @return true if the selected cards form a valid group for removal;
+     *         false otherwise.
+     */
+    public boolean isLegal(List<Integer> selectedCards) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+    }
+
+    /**
+     * Determine if there are any legal plays left on the board.
+     * In Elevens, there is a legal play if the board contains
+     * (1) a pair of non-face cards whose values add to 11, or (2) a group
+     * of three cards consisting of a jack, a queen, and a king in some order.
+     * @return true if there is a legal play left on the board;
+     *         false otherwise.
+     */
+    public boolean anotherPlayIsPossible() {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+    }
+
+
+    /**
+     * Deal cards to this board to start the game.
+     */
+    private void dealMyCards() {
+        for (int k = 0; k < cards.length; k++) {
+            cards[k] = deck.deal();
+        }
+    }
 
 	/**
 	 * Check for an 11-pair in the selected cards.
